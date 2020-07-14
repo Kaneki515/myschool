ac1fce5162418f7188bf762643e7fdceb994b6bc
Hideki <61509991+Kaneki515@users.noreply.github.com>
2020-07-14 20:04:35 -0300

    Commit tarefa 14/04/2020

---
+++ js/function-menu.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+            $('#add-disciplina').click(function(e) {
+                    e.preventDefault()
+                    $('#cont).empty()
+                        $('#cont').load('src/disciplinas/visao/adiciona-disciplinas.html')
+                    })
+
+            })
